04525499d84b7b29a46f11c320de7e4da5538d61
Carlos Armando Ochoa Ginera <caog.freeman@gmail.com>
2017-03-12 16:25:00 -0600

    Update mysql_login.php

--- Web Services Alumnos/mysql_login.php
+++ Web Services Alumnos/mysql_login.php
@@ -6,5 +6,5 @@
 define("HOSTNAME", "localhost");// Nombre del host
 define("DATABASE", "u600131417_prueb"); // Nombre de la base de datos
 define("USERNAME", "u600131417_vicer"); // Nombre del usuario
-define("PASSWORD", "vicersoftware"); // Nombre de la constraseña
+define("PASSWORD", "vicersoft"); // Nombre de la constraseña
 ?>
