e9b16f65b6453209d4ca185057d3161b48ed6a50
Carlos Armando Ochoa Ginera <caog.freeman@gmail.com>
2017-03-12 16:57:49 -0600

    Update insertar_usuario.php

--- Web Services Alumnos/insertar_usuario.php
+++ Web Services Alumnos/insertar_usuario.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $body = json_decode(file_get_contents("php://input"), true);
 
     // Insertar Alumno
-    $retorno = Usuarios::insert(
+    $retorno = usuario::insert(
         $body['email'],
 	$body['nombre'],
 	$body['sobrenombre'],
